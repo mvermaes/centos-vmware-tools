a74f28b64f63ff4644d546498e511dbf01e659a0
Michael Vermaes <mvermaes@gmail.com>
2017-08-03 14:25:06 +0800

    Better to use the CentOS 7 box when testing CentOS 7 ...

--- packer/update-local-boxes.sh
+++ packer/update-local-boxes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 c6="$(ls -1rt centos6*.box | tail -1)"
-c7="$(ls -1rt centos6*.box | tail -1)"
+c7="$(ls -1rt centos7*.box | tail -1)"
 
 vagrant box add "${c6}" --name centos6-vmware-tools --force
 vagrant box add "${c7}" --name centos7-vmware-tools --force
